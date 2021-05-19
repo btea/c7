94cef414edd2497b99c24d60c7661d40d973d1c2
c7js <c7js@qq.com>
2021-05-19 19:58:15 +0800

    C7

---
+++ src/compiler/compiler.js
@@ -0,0 +1,31 @@
+const C7Parser = require('./parser/c7-parser')
+const StyleParser = require('./parser/style-parser')
+
+
+class Compiler {
+    constructor(sourceCode) {
+        let { style, template, script} = new C7Parser(sourceCode).parse()
+        this.style = new StyleParser(style).parse()
+        this.template = this.templateFilter(template)
+        this.script = (new Function(script))()
+    }
+
+    compile() {
+        return {
+            style: this.style,
+            template: this.template,
+            script: this.script,
+        }
+    }
+
+    templateFilter(component) {
+        component.children = component.children.filter(child => child.type === 'component')
+        for (let child of component.children) {
+            this.templateFilter(child)
+        }
+        return component
+    }
+}
+
+
+module.exports = Compiler
